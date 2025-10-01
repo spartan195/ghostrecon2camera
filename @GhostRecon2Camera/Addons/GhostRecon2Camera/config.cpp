ab20ef4ea7da286af0681143082b7a1cb562027c
Xavi <xavi_ss_tgn@hotmail.com>
2025-10-01 16:37:03 +0200

    Add files via upload

---
+++ @GhostRecon2Camera/Addons/GhostRecon2Camera/config.cpp
@@ -0,0 +1,24 @@
+// config.cpp
+class CfgPatches
+{
+    class My3rdPersonCam
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 0.1;
+        requiredAddons[] = { "A3_Characters_F" };
+    };
+};
+
+class CfgVehicles
+{
+    class Man;
+    class CAManBase: Man
+    {
+        // extCameraPosition[] = { X, Y, Z };
+        // X Values: Postitive move to the left from the center, negative to the right;
+        // Y Values: Positive raises the camera (less visible character), negative lowers it;
+        // Z Values: Positive moves it forward (zoom), negative moves it back
+        extCameraPosition[] = {0.3, -0.05, -1.4};
+    };
+};
---
+++ @GhostRecon2Camera/Addons/mod.cpp
@@ -0,0 +1,8 @@
+name = "Ghost Recon 2 Camera";
+picture = "";
+logo = "";
+logoOver = "";
+tooltip = "A true Ghost Recon 2 Camera from OG Xbox";
+actionName = "Website";
+action = "https://example.com";
+overview = "Adjusts the third-person camera position for infantry (client-side).";
